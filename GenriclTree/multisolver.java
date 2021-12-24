bdcfc45ef934666131c52246ed01ea6c8bfee99c
Manas64-droid <83745970+Manas64-droid@users.noreply.github.com>
2021-12-24 13:27:09 +0530

    iterator genric tree

--- GenriclTree/multisolver.java
+++ GenriclTree/multisolver.java
@@ -1,6 +1,68 @@
 package GenriclTree;
 import java.util.*;
+
 public class multisolver{
+    public static class GenericTree implements Iterable<Integer>{
+        Node root;
+        GenericTree(Node root){
+            this.root=root;
+        }
+        @Override
+        public Iterator<Integer> iterator() {
+            // TODO Auto-generated method stub
+            Iterator<Integer> obj=new GenriclTreePreOrder(root);
+            return obj;
+        }
+        
+    }
+    public static class GenriclTreePreOrder implements Iterator<Integer>{
+        Integer nextValue;
+        Stack<Pair> st;
+        public GenriclTreePreOrder(Node root){
+            st=new Stack<>();
+            st.push(new Pair(root, -1));
+            next();
+        }
+        @Override
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            if(nextValue==null){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        @Override
+        public Integer next() {
+            // TODO Auto-generated method stub
+            //! return current value and set current value for next operation
+            Integer currentValue=nextValue;
+            //! moves nextValue forward, if not possiable make it null;
+            nextValue=null;
+            while(st.size()>0){
+                Pair top=st.peek();
+                if(top.state==-1){
+                    // pre oreder < print inc top.state > 
+                    nextValue=top.node.data;
+                    top.state++;
+                    break;
+                }
+                else if(top.state==top.node.children.size()){
+                    // post order <print pop st >
+                    st.pop();
+                }
+                else{
+                    // make child pair < push childPair in stack inc top.state >
+                    Pair childPair=new Pair(top.node.children.get(top.state), -1);
+                    st.push(childPair);
+                    top.state++;
+                }
+            }
+            return currentValue;
+        }
+
+    }
     public static class Node{
         int data;
         Node(){
@@ -11,7 +73,14 @@ public class multisolver{
         }
         ArrayList<Node> children=new ArrayList<>();
     }
-
+    public static class Pair{
+        Node node;
+        int state;
+        public Pair(Node node,int state){
+            this.node=node;
+            this.state=state;
+        }
+    }
     public static Node constrRoot(int arr[]){
         Node root=null;
         Stack<Node> st=new Stack<>();
@@ -61,7 +130,6 @@ public class multisolver{
         int arr[]={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
         Node root=constrRoot(arr);
         // display(root);
-
         size=0;
         min=Integer.MAX_VALUE;
         max=Integer.MIN_VALUE;
@@ -71,5 +139,18 @@ public class multisolver{
         System.out.println("Max->"+max);
         System.out.println("Min->"+min);
         System.out.println("Height->"+height);
+
+        GenericTree gt=new GenericTree(root);
+        Iterator<Integer> gt1=gt.iterator();
+
+        System.out.println("By using for loop");
+        for(int i:gt){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println("By usinng Iterator");
+        while(gt1.hasNext()==true){
+            System.out.print(gt1.next()+" ");
+        }
     }
 }
